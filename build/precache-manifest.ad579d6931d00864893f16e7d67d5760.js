64786e195c6c8b3d2a595be4287e09c274f9280f
Aidana Aidlanova <44188496+Aidanaaidlanova@users.noreply.github.com>
2019-02-21 21:38:23 +0600

    new version with alerts

---
+++ build/precache-manifest.ad579d6931d00864893f16e7d67d5760.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "239eba57e3e85fcc88ff838ab9ca9a1a",
+    "url": "/static/media/kotlin.239eba57.svg"
+  },
+  {
+    "revision": "30b966b891eab2e76519",
+    "url": "/static/css/main.90369a18.chunk.css"
+  },
+  {
+    "revision": "234d1f2df5bbaa59896c",
+    "url": "/static/js/1.234d1f2d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ff77b3edaab0d0eddbfd283f54e7de10",
+    "url": "/static/media/back-ellipse.ff77b3ed.svg"
+  },
+  {
+    "revision": "051170b30c189fd1413d866aa999c369",
+    "url": "/static/media/front-elips.051170b3.svg"
+  },
+  {
+    "revision": "d7732c43a9916cc02bbaa82fc16e3454",
+    "url": "/static/media/neobis.d7732c43.svg"
+  },
+  {
+    "revision": "ccfa1906ef4701e1f29bac54f8a84ffd",
+    "url": "/static/media/apple-icon.ccfa1906.svg"
+  },
+  {
+    "revision": "5f02831a1cce4447534e2dd9fc316f00",
+    "url": "/static/media/android-logo.5f02831a.svg"
+  },
+  {
+    "revision": "f95fff8c5c62b68bb394700f5129f30b",
+    "url": "/static/media/browser-icon.f95fff8c.svg"
+  },
+  {
+    "revision": "35b8308e6f31985aa08606826e4589e2",
+    "url": "/static/media/terminal-icon.35b8308e.svg"
+  },
+  {
+    "revision": "fe649602a08a63ded65c9156fdf42193",
+    "url": "/static/media/layout-icon.fe649602.svg"
+  },
+  {
+    "revision": "5759b87072ada4f4aaf04ace43c4f1fe",
+    "url": "/static/media/game-controller.5759b870.svg"
+  },
+  {
+    "revision": "84f911fa5fbc86ba74fe1bac82cab542",
+    "url": "/static/media/ar-glasses.84f911fa.svg"
+  },
+  {
+    "revision": "dd4e86828cf7792798e8e9022371903f",
+    "url": "/static/media/project-management.dd4e8682.svg"
+  },
+  {
+    "revision": "c114071667f7f3ab4a637b55ab122488",
+    "url": "/static/media/ipo.c1140716.svg"
+  },
+  {
+    "revision": "ecb0eb50edaa89d3bc9d4a1ce9dffb3f",
+    "url": "/static/media/java.ecb0eb50.svg"
+  },
+  {
+    "revision": "f0ff2f2eb579dd9abc45f7fcd9ce43b4",
+    "url": "/static/media/js.f0ff2f2e.svg"
+  },
+  {
+    "revision": "30b966b891eab2e76519",
+    "url": "/static/js/main.30b966b8.chunk.js"
+  },
+  {
+    "revision": "945893858b58ff2947081f015e88ff68",
+    "url": "/static/media/python.94589385.svg"
+  },
+  {
+    "revision": "721e5b52b8ad87d3d76ca967675dd63b",
+    "url": "/static/media/react.721e5b52.svg"
+  },
+  {
+    "revision": "fd2ed07d57ff5631468b0c66083c0a23",
+    "url": "/static/media/swift.fd2ed07d.svg"
+  },
+  {
+    "revision": "4a62af6e8d916b5ca30fe17428bf8fdf",
+    "url": "/static/media/tree-illustration.4a62af6e.svg"
+  },
+  {
+    "revision": "de78d5f34796175065f54ab3f97de6a2",
+    "url": "/static/media/phone-illustration.de78d5f3.svg"
+  },
+  {
+    "revision": "ccb5e3f675a07f03bbaf573d0c29afb7",
+    "url": "/static/media/tablet-illustration.ccb5e3f6.svg"
+  },
+  {
+    "revision": "5cfc848394b0403497547d4a642d74bb",
+    "url": "/static/media/Emir.5cfc8483.png"
+  },
+  {
+    "revision": "b8e2ddc6775b1f493df6127167e4968e",
+    "url": "/static/media/samat.b8e2ddc6.png"
+  },
+  {
+    "revision": "16a31182d0c17db73b847a9d93b5acb6",
+    "url": "/static/media/aisalkyn.16a31182.png"
+  },
+  {
+    "revision": "febdb827bb40b17a6920ddf916bb822c",
+    "url": "/static/media/mamur.febdb827.png"
+  },
+  {
+    "revision": "52964f3750075a905cd8250a56ab6299",
+    "url": "/static/media/01-gears.52964f37.svg"
+  },
+  {
+    "revision": "f1939cecc4b286dd9eeeab3a657cb908",
+    "url": "/static/media/02-bulb.f1939cec.svg"
+  },
+  {
+    "revision": "75b923bb71fed1a483c31b25edbce075",
+    "url": "/static/media/03-develop.75b923bb.svg"
+  },
+  {
+    "revision": "a3f4b69b7e4cc293ae8b3d23d034bc1e",
+    "url": "/static/media/instagram.a3f4b69b.svg"
+  },
+  {
+    "revision": "b545ecef4d15217993f30b02c0ada9f3",
+    "url": "/static/media/linkedin-logo.b545ecef.svg"
+  },
+  {
+    "revision": "1d9eec3d05362f9938337fa46639d452",
+    "url": "/static/media/facebook-logo.1d9eec3d.svg"
+  },
+  {
+    "revision": "5616dd926a4f3eb350ba68d6e34105b2",
+    "url": "/index.html"
+  }
+];
